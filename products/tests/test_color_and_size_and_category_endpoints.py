00de5b4daa314224aa1a716c76099b8e63f9ba95
amir <amirmadani901@gmail.com>
2026-06-03 13:23:26 +0330

    add tests for category, color and size endpoints

---
+++ products/tests/test_color_and_size_and_category_endpoints.py
@@ -0,0 +1,198 @@
+import pytest
+from django.urls import reverse
+from .test_products_endpoints import admin_user, normal_user, api_client
+from products.models.sizes import Size
+from products.models.colors import Color
+from products.models.categories import Category
+
+
+@pytest.fixture
+def objects_map(db):
+    color = Color.objects.create(name="test")
+    size = Size.objects.create(name="test")
+    category = Category.objects.create(name="test")
+    return {
+        "color-detail": color.pk,
+        "size-detail": size.pk,
+        "category-detail": category.pk,
+    }
+
+
+@pytest.mark.django_db
+class TestColorSIzeCategoryAPI:
+
+    @pytest.mark.parametrize(
+        "url_name",
+        [
+            "color-list",
+            "size-list",
+            "category-list",
+        ],
+    )
+    def test_list_response_200_status(self, api_client, url_name):
+        url = reverse(url_name)
+        response = api_client.get(url)
+        assert response.status_code == 200
+
+    @pytest.mark.parametrize(
+        "url_name",
+        [
+            "color-list",
+            "size-list",
+            "category-list",
+        ],
+    )
+    def test_create_with_anonymous_response_401_status(self, api_client, url_name):
+        url = reverse(url_name)
+        data = {"name": "test"}
+        response = api_client.post(url, data)
+        assert response.status_code == 401
+
+    @pytest.mark.parametrize(
+        "url_name",
+        [
+            "color-list",
+            "size-list",
+            "category-list",
+        ],
+    )
+    def test_create_with_normal_user_response_403_status(
+        self, api_client, normal_user, url_name
+    ):
+        api_client.force_authenticate(user=normal_user)
+        url = reverse(url_name)
+        data = {"name": "test"}
+        response = api_client.post(url, data)
+        assert response.status_code == 403
+
+    @pytest.mark.parametrize(
+        "url_name",
+        [
+            "color-list",
+            "size-list",
+            "category-list",
+        ],
+    )
+    def test_create_with_admin_user_response_201_status(
+        self, api_client, admin_user, url_name
+    ):
+        api_client.force_authenticate(user=admin_user)
+        url = reverse(url_name)
+        data = {"name": "test"}
+        response = api_client.post(url, data)
+        assert response.status_code == 201
+
+    @pytest.mark.parametrize(
+        "url_name",
+        [
+            "color-detail",
+            "size-detail",
+            "category-detail",
+        ],
+    )
+    def test_detail_with_anonymous_user_response_200_status(
+        self, api_client, url_name, objects_map
+    ):
+        url = reverse(url_name, kwargs={"pk": objects_map[url_name]})
+        response = api_client.get(url)
+        assert response.status_code == 200
+
+    @pytest.mark.parametrize(
+        "url_name",
+        [
+            "color-detail",
+            "size-detail",
+            "category-detail",
+        ],
+    )
+    def test_update_detail_with_normal_user_response_403_status(
+        self, api_client, url_name, objects_map, normal_user
+    ):
+        api_client.force_authenticate(user=normal_user)
+        url = reverse(url_name, kwargs={"pk": objects_map[url_name]})
+        data = {"name": "test"}
+        response = api_client.put(url, data)
+        assert response.status_code == 403
+
+    @pytest.mark.parametrize(
+        "url_name",
+        [
+            "color-detail",
+            "size-detail",
+            "category-detail",
+        ],
+    )
+    def test_update_detail_with_anonymous_user_response_401_status(
+        self, api_client, url_name, objects_map
+    ):
+        url = reverse(url_name, kwargs={"pk": objects_map[url_name]})
+        data = {"name": "test"}
+        response = api_client.put(url, data)
+        assert response.status_code == 401
+
+    @pytest.mark.parametrize(
+        "url_name",
+        [
+            "color-detail",
+            "size-detail",
+            "category-detail",
+        ],
+    )
+    def test_update_detail_with_admin_user_response_200_status(
+        self, api_client, url_name, objects_map, admin_user
+    ):
+        api_client.force_authenticate(user=admin_user)
+        url = reverse(url_name, kwargs={"pk": objects_map[url_name]})
+        data = {"name": "test"}
+        response = api_client.put(url, data)
+        assert response.status_code == 200
+
+    @pytest.mark.parametrize(
+        "url_name",
+        [
+            "color-detail",
+            "size-detail",
+            "category-detail",
+        ],
+    )
+    def test_delete_detail_with_anonymous_user_response_401_status(
+        self, api_client, url_name, objects_map
+    ):
+        url = reverse(url_name, kwargs={"pk": objects_map[url_name]})
+        data = {"name": "test"}
+        response = api_client.delete(url, data)
+        assert response.status_code == 401
+
+    @pytest.mark.parametrize(
+        "url_name",
+        [
+            "color-detail",
+            "size-detail",
+            "category-detail",
+        ],
+    )
+    def test_delete_detail_with_normal_user_response_403_status(
+        self, api_client, url_name, objects_map, normal_user
+    ):
+        api_client.force_authenticate(user=normal_user)
+        url = reverse(url_name, kwargs={"pk": objects_map[url_name]})
+        data = {"name": "test"}
+        response = api_client.delete(url, data)
+        assert response.status_code == 403
+
+    @pytest.mark.parametrize(
+        "url_name",
+        [
+            "color-detail",
+            "size-detail",
+            "category-detail",
+        ],
+    )
+    def test_delete_detail_with_admin_user_response_204_status(
+        self, api_client, url_name, objects_map, admin_user
+    ):
+        api_client.force_authenticate(user=admin_user)
+        url = reverse(url_name, kwargs={"pk": objects_map[url_name]})
+        data = {"name": "test"}
+        response = api_client.delete(url, data)
+        assert response.status_code == 204
--- products/tests/test_products_endpoints.py
+++ products/tests/test_products_endpoints.py
@@ -25,6 +25,16 @@ def admin_user():
 
 
 @pytest.fixture
+def random_product():
+    product = Product.objects.create(
+        title="test",
+        price=100.00,
+        description="test`111",
+    )
+    return product
+
+
+@pytest.fixture
 def normal_user():
     user = User.objects.create_user(
         username="normal",
@@ -66,3 +76,79 @@ class TestProductApi:
         data = {}
         response = api_client.post(url, data)
         assert response.status_code == 400
+
+    def test_create_product_with_amin_user_respomse_201_status(
+        self, api_client, admin_user
+    ):
+        api_client.force_authenticate(user=admin_user)
+        url = reverse("product-list")
+        data = {"title": "test", "description": "test", "price": 100.00}
+        response = api_client.post(url, data)
+        assert response.status_code == 201
+
+    def test_product_detail_respomse_200_status(self, api_client, random_product):
+        url = reverse("product-detail", kwargs={"pk": random_product.pk})
+        response = api_client.get(url)
+        assert response.status_code == 200
+
+    def test_update_product_detail_with_anonymous_user_respomse_401_status(
+        self, api_client, random_product
+    ):
+        url = reverse("product-detail", kwargs={"pk": random_product.pk})
+        data = {"title": "test", "description": "test", "price": 100.00}
+        response = api_client.put(url, data)
+        assert response.status_code == 401
+
+    def test_update_product_detail_with_normal_user_respomse_403_status(
+        self, api_client, random_product, normal_user
+    ):
+        api_client.force_authenticate(user=normal_user)
+        url = reverse("product-detail", kwargs={"pk": random_product.pk})
+        data = {"title": "test", "description": "test", "price": 100.00}
+        response = api_client.put(url, data)
+        assert response.status_code == 403
+
+    def test_update_product_detail_with_admin_user_respomse_200_status(
+        self, api_client, random_product, admin_user
+    ):
+        api_client.force_authenticate(user=admin_user)
+        url = reverse("product-detail", kwargs={"pk": random_product.pk})
+        data = {
+            "title": "test",
+            "description": "test",
+            "price": 100.00,
+        }
+        response = api_client.put(url, data)
+        assert response.status_code == 200
+
+    def test_update_product_detail_with_admin_user_respomse_400_status(
+        self, api_client, random_product, admin_user
+    ):
+        api_client.force_authenticate(user=admin_user)
+        url = reverse("product-detail", kwargs={"pk": random_product.pk})
+        data = {}
+        response = api_client.put(url, data)
+        assert response.status_code == 400
+
+    def test_delete_product_detail_with_anonymous_user_respomse_401_status(
+        self, api_client, random_product
+    ):
+        url = reverse("product-detail", kwargs={"pk": random_product.pk})
+        response = api_client.delete(url)
+        assert response.status_code == 401
+
+    def test_delete_product_detail_with_normal_user_respomse_403_status(
+        self, api_client, random_product, normal_user
+    ):
+        api_client.force_authenticate(user=normal_user)
+        url = reverse("product-detail", kwargs={"pk": random_product.pk})
+        response = api_client.delete(url)
+        assert response.status_code == 403
+
+    def test_delete_product_detail_with_normal_user_respomse_204_status(
+        self, api_client, random_product, admin_user
+    ):
+        api_client.force_authenticate(user=admin_user)
+        url = reverse("product-detail", kwargs={"pk": random_product.pk})
+        response = api_client.delete(url)
+        assert response.status_code == 204
